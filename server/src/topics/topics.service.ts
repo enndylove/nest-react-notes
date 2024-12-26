d793f678f2f5811f6b9957c518298e21fae58123
enndy <91780685+enndylove@users.noreply.github.com>
2024-12-26 19:11:01 +0200

    all functionality, communication whith db

--- server/src/topics/topics.service.ts
+++ server/src/topics/topics.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class TopicsService {}
+export class TopicsService {
+  constructor(private prisma: PrismaService) {}
+
+  async createTopic(name: string, userId: string, parentId?: number) {
+    return this.prisma.topic.create({
+      data: {
+        name,
+        userId,
+        parentId,
+      },
+    });
+  }
+
+  async getTopics(userId: string) {
+    return this.prisma.topic.findMany({
+      where: { userId },
+      include: { children: true },
+    });
+  }
+
+  async updateTopic(id: number, name: string, parentId?: number) {
+    return this.prisma.topic.update({
+      where: { id },
+      data: { name, parentId },
+    });
+  }
+
+  async deleteTopic(id: number) {
+    return this.prisma.topic.delete({ where: { id } });
+  }
+
+  async updateConfidenceScore(id: number, confidenceScore: number) {
+    return this.prisma.topic.update({
+      where: { id },
+      data: { confidenceScore },
+    });
+  }
+}
+
