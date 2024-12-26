6b16a0a06b268cb06ddd97df7a1d6ae4c7cc0bfa
enndy <91780685+enndylove@users.noreply.github.com>
2024-12-26 19:47:17 +0200

    connect notes and topics modules to project

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,11 @@ import { PrismaService } from './prisma.service';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { TopicsModule } from './topics/topics.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, NotesModule, TopicsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
