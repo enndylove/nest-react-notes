5cd0d556e8fa6784f1eb060c656ee5223ed67c93
enndy <91780685+enndylove@users.noreply.github.com>
2024-12-27 02:49:18 +0200

    enableCors for client local server

---
+++ server/src/constans/cors.constans.ts
@@ -0,0 +1,5 @@
+export const CLIENT_CORS = {
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+};
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CLIENT_CORS } from './constans/cors.constans';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors(CLIENT_CORS);
+  
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
